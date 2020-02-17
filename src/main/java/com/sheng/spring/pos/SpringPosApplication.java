4b3a8c9467c79317cd98419f1b025345019e5096
shengping <shengping@gmail.com>
2020-02-17 16:41:59 +0100

    Project created.
    Project is created and defined.

---
+++ src/main/java/com/sheng/spring/pos/SpringPosApplication.java
@@ -0,0 +1,13 @@
+package com.sheng.spring.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPosApplication.class, args);
+	}
+
+}
